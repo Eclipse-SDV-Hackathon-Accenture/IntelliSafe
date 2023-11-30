95a6c32d9404abccc1e14a4552680e8f263541dd
Sreehari <55207902+sreehari59@users.noreply.github.com>
2023-11-30 09:46:40 +0100

    Update alert_notification_app.py

--- app_ui/alert_notification_app.py
+++ app_ui/alert_notification_app.py
@@ -13,7 +13,7 @@ def get_img_as_base64(file):
         data = f.read()
     return base64.b64encode(data).decode()
 
-img = get_img_as_base64("assets/background-wireframe.jpg")
+img = get_img_as_base64("../app_ui/assets/background-wireframe.jpg")
 
 page_bg_img = f"""
 <style>
